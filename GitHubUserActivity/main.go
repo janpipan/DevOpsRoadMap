ec0dcaadfc894f43b53406188679043a1b9f2244
jpipan <janpipan@gmail.com>
2024-12-15 21:43:16 +0100

    github-activity with basic events

--- GitHubUserActivity/main.go
+++ GitHubUserActivity/main.go
@@ -10,17 +10,14 @@ import (
 )
 
 type Event struct {
-	Type    string      `json:"type"`
-	Repo    Repo        `json:"repo"`
-	Payload interface{} `json:"payload"`
+	Type    string                 `json:"type"`
+	Repo    Repo                   `json:"repo"`
+	Payload map[string]interface{} `json:"payload"`
 }
 
 type Repo struct {
 	Name string `json:"name"`
 	Url  string `json:"url"`
-}
-
-type Actitivity struct {
 }
 
 type ErrorResponse struct {
@@ -55,32 +52,112 @@ func checkGitHubUser(user string) error {
 	return nil
 }
 
-func main() {
-
+func getUserEvents(user string) ([]Event, error) {
 	// check if github user exists
-	if err := checkGitHubUser("janpipan"); err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+	if err := checkGitHubUser(user); err != nil {
+		// fmt.Print(err.Error())
+		// os.Exit(1)
+		return nil, err
 	}
 
+	// create url
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", user)
+
 	// make http get request
-	response, err := http.Get("https://api.github.com/users/janpipan/events")
+	response, err := http.Get(url)
 
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		return nil, err
+		// fmt.Print(err.Error())
+		// os.Exit(1)
 	}
 	// read response body
 	responseData, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+		// log.Fatal(err)
 	}
 
-	var responseObject []Event
-	json.Unmarshal(responseData, &responseObject)
+	// parse json data
+	var responseJson []Event
+	json.Unmarshal(responseData, &responseJson)
 
-	for _, event := range responseObject {
-		fmt.Println(event.Payload.(map[string]interface{})[""])
+	return responseJson, nil
+
+}
+
+func printEventMessage(event Event) string {
+	payload := event.Payload
+	switch event.Type {
+	case "CommitCommentEvent":
+		return fmt.Sprintf("Create a comment %s in %s repo", payload["comment"], event.Repo.Name)
+	case "CreateEvent":
+		if payload["ref_type"] == "branch" {
+			return fmt.Sprintf("Created a branch %s in repo %s", payload["ref"], event.Repo.Name)
+		} else if payload["ref_type"] == "tag" {
+			return fmt.Sprintf("Created a tag %s in repo %s", payload["ref"], event.Repo.Name)
+		} else {
+			return fmt.Sprintf("Created a repository %s", event.Repo.Name)
+		}
+	case "DeleteEvent":
+		if payload["ref_type"] == "branch" {
+			return fmt.Sprintf("Deleted a branch %s in repo %s", payload["ref"], event.Repo.Name)
+		} else if payload["ref_type"] == "tag" {
+			return fmt.Sprintf("Deleted a tag %s in repo %s", payload["ref"], event.Repo.Name)
+		}
+		return ""
+	case "ForkEvent":
+		return fmt.Sprintf("Forked a repository %s", event.Repo.Name)
+	case "GollumEvent":
+		return "GollumEvent"
+	case "IssueCommentEvent":
+		return "IssueCommentEvent"
+	case "IssuesEvent":
+		return "IssuesEvent"
+	case "MemberEvent":
+		return "MemberEvent"
+	case "PublicEvent":
+		return "PublicEvent"
+	case "PullRequestEvent":
+		return "PullRequestEvent"
+	case "PullRequestReviewEvent":
+		return "PullRequestReviewEvent"
+	case "PullRequestReviewCommentEvent":
+		return "PullRequestReviewCommentEvent"
+	case "PullRequestReviewThreadEvent":
+		return "PullRequestReviewThreadEvent"
+	case "PushEvent":
+		return fmt.Sprintf("Pushed %d commits to %s", int(payload["size"].(float64)), event.Repo.Name)
+	case "ReleaseEvent":
+		return "ReleaseEvent"
+	case "SponsorshipEvent":
+		return "SponsorshipEvent"
+	case "WatchEvent":
+		return fmt.Sprintf("Starred %s repo", event.Repo.Name)
+	default:
+		return "Unknown activity"
+	}
+}
+
+func main() {
+
+	// check if user set input arg
+	if len(os.Args) != 2 {
+		log.Fatal("GitHub user is missing")
+	}
+
+	user := os.Args[1]
+
+	events, err := getUserEvents(user)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	for _, event := range events {
+		fmt.Println(printEventMessage(event))
+
 	}
 }
